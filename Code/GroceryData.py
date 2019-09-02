14a75db3d8531bd76c8c3a11112c9f969d3963a6
Lance Cundy <lance.cundy@gmail.com>
2019-09-02 11:06:24 -0500

    Update GroceryData.py

--- Code/GroceryData.py
+++ Code/GroceryData.py
@@ -5,7 +5,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 Years = ['2004', '2005', '2006', '2007','2008', '2009', '2010', '2011', '2012', '2013', '2014']
-panelistsCols = ['household_code', 'panel_year', 'projection_factor', 'projection_factor_magnet', 'household_income', 'household_size', 'type_of_residence', 'male_head_age', 'female_head_age', 'male_head_education', 'female_head_education', 'male_head_occupation', 'female_head_occupation', 'male_head_employment', 'female_head_employment', 'marital_status', 'race', 'hispanic_origin', 'fips_state_descr']
+panelistsCols = ['Household_Cd', 'Panel_Year', 'Projection_Factor', 'Projection_Factor_Magnet', 'Household_Income', 'Household_Size', 'Type_Of_Residence', 'Male_Head_Age', 'Female_Head_Age', 'Male_Head_Education', 'Female_Head_Education', 'Male_Head_Occupation', 'Female_Head_Occupation', 'Male_Head_Employment', 'Female_Head_Employment', 'Marital_Status', 'Race', 'Hispanic_Origin', 'Fips_State_Desc']
+panelistsColsNew = ['household_code', 'panel_year', 'projection_factor', 'projection_factor_magnet', 'household_income', 'household_size', 'type_of_residence', 'male_head_age', 'female_head_age', 'male_head_education', 'female_head_education', 'male_head_occupation', 'female_head_occupation', 'male_head_employment', 'female_head_employment', 'marital_status', 'race', 'hispanic_origin', 'fips_state_descr']
 tripsCols = ['trip_code_uc', 'household_code', 'retailer_code', 'purchase_date', 'panel_year', 'total_spent']
 retailersCols = ['retailer_code', 'channel_type']
 #purchasesCols = ['trip_code_uc', 'upc', 'upc_ver_uc', 'quantity', 'total_price_paid']
@@ -15,6 +16,7 @@ retailersCols = ['retailer_code', 'channel_type']
 Trips = pd.DataFrame()
 for ii in range(len(Years)):
     paneliststemp = pd.read_csv("HMS/"+Years[ii]+"/Annual_Files/panelists_"+Years[ii]+".tsv", sep='\t', usecols=panelistsCols)
+    paneliststemp.columns = panelistsColsNew
     #paneliststemp = paneliststemp[paneliststemp.projection_factor_magnet > 0]
     #paneliststemp = paneliststemp.reset_index(drop=True)
     tripstemp = pd.read_csv("HMS/"+Years[ii]+"/Annual_Files/trips_"+Years[ii]+".tsv", sep='\t', usecols=tripsCols)
@@ -63,7 +65,7 @@ GTripsR['month'] = (GTripsR['purchase_date']).dt.month
 
 GroceryTrips = pd.merge(GTripsR, IRData, how='left', on=['year', 'month', 'region'])
 
-GroceryTrips.to_csv("Datasets/GroceryTrips.csv", index=False)
+GroceryTrips.to_csv("../Datasets/GroceryTrips.csv", index=False)
 
 
 
