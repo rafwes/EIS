7dde8588a560fb9bf910d4392299d5be1b709a6d
Rafael <23530231+rafwes@users.noreply.github.com>
2020-04-21 08:15:48 -0300

    deflate consumption data

--- code_new/estimation_data.R
+++ code_new/estimation_data.R
@@ -7,3 +7,14 @@ source(file.path(base_path,"EIS/code_new/interest_rates.R"))
 source(file.path(base_path,"EIS/code_new/grocery_data.R"))
 
 
+consumption_ne_def <- 
+  consumption_ne %>% 
+  left_join(index_table %>% 
+              select(DATE,INDEX_CPI_NE),
+            by = c("PURCHASE_DATE" = "DATE")) %>% 
+  mutate(TOTAL_SPENT_DEF = 100 * TOTAL_SPENT / INDEX_CPI_NE) %>% 
+  na.exclude() %>% 
+  select(HOUSEHOLD_CODE,
+         PURCHASE_DATE,
+         TOTAL_SPENT_DEF)
+
