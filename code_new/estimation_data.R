94060ca28158fc071b1bb98529421d92ca502203
Rafael <23530231+rafwes@users.noreply.github.com>
2020-04-21 08:16:57 -0300

    comment

--- code_new/estimation_data.R
+++ code_new/estimation_data.R
@@ -6,7 +6,7 @@ base_path <- "/home/rafael/Sync/IMPA/2020.0/simulations/code"
 source(file.path(base_path,"EIS/code_new/interest_rates.R"))
 source(file.path(base_path,"EIS/code_new/grocery_data.R"))
 
-
+# Gathers inflation data and deflates consumption by region
 consumption_ne_def <- 
   consumption_ne %>% 
   left_join(index_table %>% 
@@ -17,4 +17,6 @@ consumption_ne_def <-
   select(HOUSEHOLD_CODE,
          PURCHASE_DATE,
          TOTAL_SPENT_DEF)
+
+rm(consumption_ne)
 
